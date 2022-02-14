4ddd000b5673002180002bcd7d15befbf15bf227
lenargasimov <lenargasimov@duck.com>
2022-02-14 20:22:26 +0300

    feat: run frontend in production mode

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload. Change from docker.
+          Edit <code>src/App.js</code> and save to reload. Change from docker. We are in dev.
         </p>
         <a
           className="App-link"
