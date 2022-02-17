08170bf6b76b9a57760574c3e6f14301ee0323b5
lenargasimov <lenargasimov@duck.com>
2022-02-17 14:51:17 +0300

    feat: added frontend proxy

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,14 @@
 import logo from './logo.svg';
+import axios from 'axios';
 import './App.css';
 
 function App() {
+  const makeApiRequest = () => {
+    console.log('makeApiRequest');
+    axios("/api/testwithcurrentuser").then(response => {
+      console.log('response', response);
+    });
+  };
   return (
     <div className="App">
       <header className="App-header">
@@ -18,6 +25,7 @@ function App() {
           Learn React
         </a>
       </header>
+      <button onClick={makeApiRequest}>Make api request</button>
     </div>
   );
 }
